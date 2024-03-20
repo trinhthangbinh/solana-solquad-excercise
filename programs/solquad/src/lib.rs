cb629a76de2c50e1e316ebbbd19d472b20229dd7
trinhthangbinh <164190238+trinhthangbinh@users.noreply.github.com>
2024-03-20 20:47:13 +0700

    Update lib.rs

--- programs/solquad/src/lib.rs
+++ programs/solquad/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("5sFUqUTjAMJARrEafMX8f4J1LagdUQ9Y8TR8HwGNHkU8");
+declare_id!("5Q7KXHQQNaeke7ZFuqCxsFHz3LtqwGaTtQW1YiDHubtu");
 
 #[program]
 pub mod solquad {
